8692df7b235d053ed2de5f3ed24486ca63a9239f
Adrian Sikorski <radian3113@gmail.com>
2023-02-28 00:34:15 +0100

    added function

--- js/script.js
+++ js/script.js
@@ -1,70 +1,93 @@
-console.log("Hej wszystkim developerom");
+{
+  console.log("Hej wszystkim developerom");
 
 
-let formElement = document.querySelector(".js-form");
-let currencySelectionElement = document.querySelector(".js-currencySelection")
-let amountElement = document.querySelector(".js-amount");
-
-let resultPLNElement = document.querySelector(".js-resultPLN");
-let resultEURElement = document.querySelector(".js-resultEUR");
-let resultGBPElement = document.querySelector(".js-resultGBP");
-let resultUSDElement = document.querySelector(".js-resultUSD");
-
-
-let PLNtoEUR = 0.2129;
-let PLNtoGBP = 0.1877;
-let PLNtoUSD = 0.2287;
-
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let currencySelection = currencySelectionElement.value;
-  let amount = +amountElement.value;
-
-  let resultPLN;
-  let resultEUR;
-  let resultGBP;
-  let resultUSD;
+const calculateResult = (amount, currencySelection) => {
+const PLNtoEUR = 0.2129;
+const PLNtoGBP = 0.1877;
+const PLNtoUSD = 0.2287;
 
   switch (currencySelection) {
     case "PLN":
-      resultPLN = amount * 1;
-      resultEUR = amount * PLNtoEUR;
-      resultGBP = amount * PLNtoGBP;
-      resultUSD = amount * PLNtoUSD;
-      break;
+  
+      return PLN = amount * 1;
+      return EUR = amount * PLNtoEUR;
+      return GBP = amount * PLNtoGBP;
+      return USD = amount * PLNtoUSD;
+   
+     
     case "EUR":
-      resultPLN = amount / PLNtoEUR;
-      resultEUR = amount * 1;
-      resultGBP = amount * PLNtoGBP / PLNtoEUR;
-      resultUSD = amount * PLNtoUSD / PLNtoEUR;
-      break;
+      return PLN = amount / PLNtoEUR;
+      return EUR = amount * 1;
+      return GBP = amount * PLNtoGBP / PLNtoEUR;
+      return USD = amount * PLNtoUSD / PLNtoEUR;
+      
     case "GBP":
-      resultPLN = amount / PLNtoGBP;
-      resultEUR = amount * PLNtoEUR / PLNtoGBP;
-      resultGBP = amount * 1;
-      resultUSD = amount * PLNtoUSD / PLNtoGBP;
-      break;
+      return PLN = amount / PLNtoGBP;
+      return EUR = amount * PLNtoEUR / PLNtoGBP;
+      return GBP = amount * 1;
+      return USD = amount * PLNtoUSD / PLNtoGBP;
+      
     case "USD":
-      resultPLN = amount / PLNtoUSD;
-      resultEUR = amount * PLNtoEUR / PLNtoUSD;
-      resultGBP = amount * PLNtoGBP / PLNtoUSD;
-      resultUSD = amount * 1;
-      break;
+      return PLN = amount / PLNtoUSD;
+      return EUR = amount * PLNtoEUR / PLNtoUSD;
+      return GBP = amount * PLNtoGBP / PLNtoUSD;
+      return USD = amount * 1;
+      
   }
-
+}
+const updateResultText =(resultPLN,resultEUR,resultGBP,resultUSD) =>{
+  const resultPLNElement = document.querySelector(".js-resultPLN");
+  const resultEURElement = document.querySelector(".js-resultEUR");
+  const resultGBPElement = document.querySelector(".js-resultGBP");
+  const resultUSDElement = document.querySelector(".js-resultUSD");
+  
   resultPLNElement.innerText = resultPLN.toFixed(2);
-
-
   resultEURElement.innerText = resultEUR.toFixed(2);
-
-
-
   resultGBPElement.innerText = resultGBP.toFixed(2);
+  resultUSDElement.innerText = resultUSD.toFixed(2);
+}
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  
+  const currencySelectionElement = document.querySelector(".js-currencySelection")
+  const amountElement = document.querySelector(".js-amount");
+  
+  
+
+  const currencySelection = currencySelectionElement.value;
+  const amount = +amountElement.value;
+
+  const resultPLN = calculateResult(amount, currencySelection);
+  const resultEUR = calculateResult(amount, currencySelection);
+  const resultGBP = calculateResult(amount, currencySelection);
+  const resultUSD = calculateResult(amount, currencySelection);
+
+  updateResultText(resultPLN,resultEUR,resultGBP,resultUSD);
+
+  onFormSubmit();
 
   
 
-  resultUSDElement.innerText = resultUSD.toFixed(2);
 
-})
+  
+
+
+
+ 
+
+  
+
+  
+
+}
+
+const init =() => {
+  const formElement = document.querySelector(".js-form");
+
+formElement.addEventListener("submit", onFormSubmit)
+}
+init();
+
+}
