5e896d2b4d3844530ddfef0033a6f2f04fec3ef3
Adrian Sikorski <radian3113@gmail.com>
2023-03-18 20:17:53 +0100

    cosmetic changes

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 
 
   const calculateResult = (amount, currencySelection) => {
-
     const PLNtoEUR = 0.2129;
     const PLNtoGBP = 0.1877;
     const PLNtoUSD = 0.2287;
+
     let resultPLN;
     let resultEUR;
     let resultGBP;
@@ -57,12 +57,11 @@
     resultEURElement.innerText = resultEUR.toFixed(2);
     resultGBPElement.innerText = resultGBP.toFixed(2);
     resultUSDElement.innerText = resultUSD.toFixed(2);
-
-
   }
 
   const onFormSubmit = (event) => {
     event.preventDefault();
+
     const currencySelectionElement = document.querySelector(".js-currencySelection")
     const amountElement = document.querySelector(".js-amount");
     const currencySelection = currencySelectionElement.value;
@@ -70,7 +69,6 @@
 
     calculateResult(amount, currencySelection);
   }
-
 
   const init = () => {
     const formElement = document.querySelector(".js-form");
